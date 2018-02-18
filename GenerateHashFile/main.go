edcb7cef55e3588a7cf63d79aac27f84a16850c7
Akshay N V <akshayvenkatesha@gmail.com>
2018-02-18 08:27:25 +0530

    added slice the data function and its corresponding test cases

--- GenerateHashFile/main.go
+++ GenerateHashFile/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"crypto/sha1"
+	"errors"
 	"fmt"
 )
 
@@ -12,4 +13,34 @@ func main() {
 func GenerateHash(data []byte) []byte {
 	hash := sha1.Sum(data)
 	return hash[:]
+}
+
+func SliceTheData(data []byte, sliceSize int) ([][]byte, error) {
+
+	totalDataLength := len(data)
+	var index int
+	var slicedData [][]byte
+	var exception error
+
+	if sliceSize == 0 {
+		fmt.Print("sliceSize should be greater than 0")
+		exception = errors.New("sliceSize should be greater than 0")
+		return nil, exception
+	}
+	if totalDataLength == 0 {
+		fmt.Print("Data size is 0")
+		exception = errors.New("Data size is 0")
+		return nil, exception
+	}
+
+	for totalDataLength >= (index + sliceSize) {
+		slicedData = append(slicedData, data[index:(index+sliceSize)])
+		index += sliceSize
+	}
+
+	if totalDataLength > index {
+		slicedData = append(slicedData, data[index:totalDataLength-1])
+	}
+
+	return slicedData, nil
 }
--- GenerateHashFile/main_test.go
+++ GenerateHashFile/main_test.go
@@ -2,6 +2,9 @@ package main
 
 import (
 	"encoding/hex"
+	"errors"
+	"fmt"
+	"math/rand"
 	"strings"
 	"testing"
 )
@@ -32,4 +35,69 @@ func TestGenerateHash256KB(t *testing.T) {
 	if result != 0 {
 		t.Errorf("Expected %s but found %s", expectedhash, actualHash)
 	}
+}
+
+func TestSliceTheDataUnEvenData(t *testing.T) {
+	_ = ValidateDataSlicing(RandString(23), 2)
+}
+
+func TestSliceTheDataLessThanSliseSize(t *testing.T) {
+	_ = ValidateDataSlicing(RandString(23), 31)
+}
+
+func TestSliceValidateZeroData(t *testing.T) {
+	err := ValidateDataSlicing("", 2)
+	if err == nil {
+		t.Errorf("Expected error but did not find")
+		return
+	}
+}
+func TestSliceValidateZeroSliceLen(t *testing.T) {
+	err := ValidateDataSlicing(RandString(20), 0)
+	if err == nil {
+		t.Errorf("Expected error but did not find")
+		return
+	}
+}
+
+func ValidateDataSlicing(testString string, slicingSize int) error {
+
+	testStringBytes := []byte(testString)
+	slicedData, err := SliceTheData(testStringBytes, slicingSize)
+	actualDataSize := len(slicedData)
+
+	if err != nil {
+		return err
+	}
+
+	expectedDataSize := GetExpectedDataSize(testStringBytes, slicingSize)
+	if expectedDataSize != actualDataSize {
+		errorMessage := fmt.Sprintf("Expected data size %d but found %d", expectedDataSize, actualDataSize)
+		return errors.New(errorMessage)
+	}
+	return nil
+
+}
+
+func GetExpectedDataSize(data []byte, slicesize int) int {
+	var expectedDataSize int
+	dataLength := len(data)
+	remainder := dataLength % slicesize
+
+	expectedDataSize = dataLength / slicesize
+
+	if remainder != 0 {
+		expectedDataSize++
+	}
+	return expectedDataSize
+}
+
+const letterBytes = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
+func RandString(n int) string {
+	b := make([]byte, n)
+	for i := range b {
+		b[i] = letterBytes[rand.Intn(len(letterBytes))]
+	}
+	return string(b)
 }
